151e6ab005e3b196162d825baac0f835c713d43c
betsyvies <ing.betsyvies@gmail.com>
2018-02-07 16:10:31 -0500

    añadiendo incializacion con firebase

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-// La funcionalidad de tu proyecto
+$(document).ready(function() {
+  // Login
+  var $loginGoogle = $('#log-in-google');
+
+  $loginGoogle.on('click', googleLogin);
+
+  function googleLogin() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+  
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // The signed-in user info.
+      var user = result.user;
+      // Mostramos su contenido
+      console.log(user);
+      // Llamamos a la funcion
+      saveData(result.user);
+    });
+  }
+
+  function saveData(user) {
+    var users = {
+      uid: user.uid,
+      name: user.displayName,
+      email: user.email,
+      photo: user.photoURL
+    };
+    firebase.database().ref('bd/' + user.uid).set(users);
+  }
+});
