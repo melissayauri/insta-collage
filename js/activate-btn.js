61de17e1881bb64274b695ad970f32fb99a2eaca
Araceli Cueva <aycuevam@gmail.com>
2018-02-07 23:43:23 -0500

    Subiendo archivos js

---
+++ js/activate-btn.js
@@ -0,0 +1,10 @@
+$(document).ready(() =>{
+  cl = console.log;
+  activeButton = (btn) =>{
+    if(validateEmail === true &&  validatePassword === true){
+      btn.removeAttr('disabled');
+    }else {
+      btn.attr('disabled', 'disabled');
+    }
+  }
+});
