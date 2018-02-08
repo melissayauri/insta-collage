ddd337a96679708736ca56e8b04daab8f4644e3b
melissayauri <yaurimelissa@gmail.com>
2018-02-08 09:08:45 -0500

    elimnando error parentnode

--- js/function-collage.js
+++ js/function-collage.js
@@ -3,6 +3,23 @@ window.addEventListener('load', function(event) {
   /* variables de los atributos al container y las imagenes*/
   let draggable = document.querySelectorAll('[draggable]');
   let containers = document.querySelectorAll('[data-container ]');
+
+  function selection(event) {
+    event.preventDefault();
+   if(event.type != "drop"){
+      console.log(event.type);
+      return
+    }
+    let elementId = event.dataTransfer.getData('text');
+    let element = document.getElementById(elementId);
+    if (element.parentNode === this) {
+      this.className = '';
+      return;
+    }
+    element.parentNode.removeChild(element);
+    this.appendChild(element);
+    this.className = '';
+  }
   /* ejecutando los eventos*/
   draggable.forEach(function(value, i) {
     draggable[i].addEventListener('dragstart', dragstart);
@@ -14,16 +31,4 @@ window.addEventListener('load', function(event) {
   function dragstart(event) {
     event.dataTransfer.setData('text', this.id);
   };
-  function selection(event) {
-    event.preventDefault();
-    let elementId = event.dataTransfer.getData('text');
-    let element = document.getElementById(elementId);
-    if (element.parentNode === this) {
-      this.className = '';
-      return;
-    }
-    element.parentNode.removeChild(element);
-    this.appendChild(element);
-    this.className = '';
-  }
 });
